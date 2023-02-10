249510c6e0b494d365f42e0279307d19d410c923
DorukEmre <doruk.emre@outlook.com>
2023-02-10 14:57:34 +0000

    init commit - working graphql query to static file

---
+++ README.md
@@ -0,0 +1,11 @@
+Frontend:
+React framework
+Apollo client
+
+Backend:
+GraphQL API
+Node.js, express-graphql
+Mongoose
+
+Database:
+MongoDB
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+// graphQL end point '/graphql'
+// graphqlHTTP require schema
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    // graphiql true if in dev mode to use GraphiQL tool to manually issue GraphQL queries
+    graphiql: process.env.NODE_ENV === 'development',
+  }),
+)
+
+app.listen(port, console.log(`Server running  http://localhost:${port}`))
