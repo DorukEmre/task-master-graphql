30cce5fec7089786e45dfc783e8325d3526a2b53
DorukEmre <doruk.emre@outlook.com>
2023-02-13 12:23:37 +0000

    Cleared css styles

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Task Manager</title>
-        <meta name="description" content="Manage your tasks" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main className={styles.main}>Hello</main>
+      <main className="container">
+        <h1>Hello</h1>
+      </main>
     </>
   )
 }
