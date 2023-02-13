f0d73eaec27ab9a048814c8b71ae5def1d15d779
DorukEmre <doruk.emre@outlook.com>
2023-02-13 15:56:26 +0000

    Added Delete Client function

--- client/src/components/ClientRow.tsx
+++ client/src/components/ClientRow.tsx
@@ -1,13 +1,39 @@
+import { useMutation } from '@apollo/client'
+import { DELETE_CLIENT } from '@/mutations/clientMutations'
+import { GET_CLIENTS } from '@/queries/clientQueries'
 import { Client } from '@/types'
 
 const ClientRow = ({ client }: { client: Client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // Update cache by setting the data to the response of deleteClient
+    update(cache, { data: { deleteClient } }) {
+      // Get query from the cache (not a whole new query)
+      const { clients }: { clients: Client[] } = cache.readQuery({
+        query: GET_CLIENTS,
+      })!
+      // Write to cache setting the data to filter out the client that we deleted
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id !== deleteClient.id),
+        },
+      })
+    },
+  })
+
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">Delete</button>
+        <button
+          className="btn btn-danger btn-sm"
+          onClick={() => deleteClient()}
+        >
+          Delete
+        </button>
       </td>
     </tr>
   )
